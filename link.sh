11d2f5f1f4007f0fd27cde0c3e0c2402b105c2e8
Martin Balfanz <mbalfanz@mozilla.com>
2018-03-10 23:58:28 +0100

    More robust link.sh

--- link.sh
+++ link.sh
@@ -7,5 +7,12 @@ dotfiles=("chunkwmrc"
           "hyper.js")
 
 for file in "${dotfiles[@]}"; do
-    ln -sf "`pwd`/${file}" "${HOME}/.${file}"
+    source="`pwd`/${file}"
+    dest="${HOME}/.${file}"
+
+    if [ -L $dest ] && [ -e $dest ]; then
+        echo $file "already linked"
+    else
+        ln -sfv $source $dest
+    fi
 done
