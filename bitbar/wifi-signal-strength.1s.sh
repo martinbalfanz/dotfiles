57639ee44190580cca134d61b3cc5ab259d580fe
Martin Balfanz <mbalfanz@mozilla.com>
2018-07-27 12:16:06 +0200

    Add bitbar script to monitor wifi signal strength

---
+++ bitbar/wifi-signal-strength.1s.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# <bitbar.title>Wifi Signal Strength</bitbar.title>
+# <bitbar.author>Martin</bitbar.author>
+# <bitbar.author.github>martinbalfanz</bitbar.author.github>
+# <bitbar.desc>Displays current wifi signal strength / RSSI</bitbar.desc>
+
+STRENGTH=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep CtlRSSI | sed "s/agrCtlRSSI://")
+
+if [ "$STRENGTH" -gt -60 ]; then
+    echo "${STRENGTH}dBm | color=green"
+elif [ "$STRENGTH" -gt -67 ]; then
+    echo "${STRENGTH}dBm | color=orange"
+else
+    echo "${STRENGTH}dBm | color=red"
+fi
+
+
+echo "---"
+/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | xargs -L1 echo
