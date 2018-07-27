18d43cbf4cb5b85dd99eceada4a91debd8a6e807
Martin Balfanz <mbalfanz@mozilla.com>
2018-07-27 15:15:41 +0200

    Add nightlight and rebel-readline

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-ancient "0.6.15"]
-                  [lein-try "0.4.3"]]
-        :dependencies [[slamhound "1.5.5"]]}}
+                  [lein-try "0.4.3"]
+                  [nightlight/lein-nightlight "RELEASE"]]
+        :dependencies [[slamhound "1.5.5"]
+                       [com.bhauman/rebel-readline "0.1.4"]]
+        :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}}
