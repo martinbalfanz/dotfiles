17b35e4e4f8583ee5f3e1c371c19c2877975c9ba
Martin Balfanz <mbalfanz@mozilla.com>
2018-08-13 10:12:44 +0200

    Add eastwood and kibit to lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[lein-pprint "1.2.0"]
                   [lein-ancient "0.6.15"]
                   [lein-try "0.4.3"]
+                  [jonase/eastwood "0.2.9"]
+                  [lein-kibit "0.1.6"]
                   [nightlight/lein-nightlight "RELEASE"]]
         :dependencies [[slamhound "1.5.5"]
                        [com.bhauman/rebel-readline "0.1.4"]]
